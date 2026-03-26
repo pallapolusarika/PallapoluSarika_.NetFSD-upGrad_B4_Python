3605e2d65e2a28f333985e4f644b74f8cf1ce387
pallapolusarika <pallapolusarikareddy13@gmail.com>
2026-03-26 22:16:30 +0530

    Day29_Handson_C# completed

---
+++ Day29_Handson_C#/Controllers/ContactController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ContactManagementApp.Models;
+
+namespace ContactManagementApp.Controllers
+{
+    public class ContactController : Controller
+    {
+        static List<ContactInfo> contacts = new List<ContactInfo>();
+
+        public ActionResult ShowContacts()
+        {
+            return View(contacts);
+        }
+
+        public ActionResult GetContactById(int id)
+        {
+            var contact = contacts.FirstOrDefault(c => c.ContactId == id);
+            return View(contact);
+        }
+
+        public ActionResult AddContact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddContact(ContactInfo contactInfo)
+        {
+            contacts.Add(contactInfo);
+            return RedirectToAction("ShowContacts");
+        }
+    }
+}
